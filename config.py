57c77ed47ad88754cfa25ed6b10ca1ac0aa81b66
ilamagarita <110303854+ilamagarita@users.noreply.github.com>
2022-07-30 19:59:30 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    role_name = "Verified *+:｡.｡"
+    message_id = "1002924053541171200"
+    prefix = "!"
+    bot_token = "MTAwMjg3MzgxNjM5MDg0MDQyMA.GF2Upx.BzGih6nyVVWsvV2vrn0EBunfcL5aFwwz-vPcnA"
+    channel_id = "1001852430998319226"
